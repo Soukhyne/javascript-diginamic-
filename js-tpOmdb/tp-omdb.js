706c39c28a2d9cd0991fe8414a658b444dc278ed
madeline <madeline.lafont@gmail.com>
2021-12-21 12:16:41 +0100

    exo cinema

--- js-tpOmdb/tp-omdb.js
+++ js-tpOmdb/tp-omdb.js
@@ -1,31 +1,34 @@
 //declaration des variables
 let input = document.getElementById("nom");
-let form = document.getElementById("form");
 let resultat = document.getElementById("resultat");
+let button = document.getElementById("button");
 let value;
+let poster;
+let h1;
+let p1;
 
 //ecoute de la recherche
-form.addEventListener("submit", function (e) {
+button.addEventListener("click", function (e) {
   e.preventDefault();
   value = input.value;
-  console.log(value);
   resultat.innerHTML = "";
 
-  fetch(`http://www.omdbapi.com/?apikey=f6e256e1&t=${value}`)
+  fetch(`http://www.omdbapi.com/?apikey=f6e256e1&s=${value}`)
     .then((res) => res.json())
     .then((data) => {
-      console.log(data);
-      console.log(data.Poster);
-      let poster = document.createElement("img");
-      poster.setAttribute("src", data.Poster);
-      poster.setAttribute("alt", data.Title);
-      poster.setAttribute("id", "poster");
-      resultat.appendChild(poster);
-      let h1 = document.createElement("h1");
-      h1.innerText = data.Title;
-      resultat.appendChild(h1);
-      let p1 = document.createElement("p");
-      p1.innerText = `Date de sortie : ${data.Released}`;
-      resultat.appendChild(p1);
+      for (let i = 0; i < data.Search.length; i++) {
+        poster = document.createElement("img");
+        poster.setAttribute("src", data.Search[i].Poster);
+        poster.setAttribute("alt", data.Search[i].Title);
+        poster.setAttribute("class", "poster");
+        resultat.appendChild(poster);
+        h1 = document.createElement("h1");
+        h1.setAttribute("class", "text-primary");
+        h1.innerText = data.Search[i].Title;
+        resultat.appendChild(h1);
+        p1 = document.createElement("p");
+        p1.innerText = `Année de sortie : ${data.Search[i].Year}`;
+        resultat.appendChild(p1);
+      }
     });
 });
